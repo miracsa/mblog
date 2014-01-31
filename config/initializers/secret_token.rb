33b6208c0940b2201672e521d11a604c9575784b
Miraç Satıç <mirac.satic@gmail.com>
2014-02-01 01:10:07 +0200

    new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/.keep 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/stylesheets/application.css 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/concerns/.keep 	new file:   app/helpers/application_helper.rb 	new file:   app/mailers/.keep 	new file:   app/models/.keep 	new file:   app/models/concerns/.keep 	new file:   app/views/layouts/application.html.erb 	new file:   bin/bundle 	new file:   bin/rails 	new file:   bin/rake 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/filter_parameter_logging.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   lib/assets/.keep 	new file:   lib/tasks/.keep 	new file:   log/.keep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/robots.txt 	new file:   test/controllers/.keep 	new file:   test/fixtures/.keep 	new file:   test/helpers/.keep 	new file:   test/integration/.keep 	new file:   test/mailers/.keep 	new file:   test/models/.keep 	new file:   test/test_helper.rb 	new file:   vendor/assets/javascripts/.keep 	new file:   vendor/assets/stylesheets/.keep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mblog::Application.config.secret_key_base = '5e9e005cb248eea388b3f8feddb06ddcf06fd638c81e8e0719bb34771fe1926ede1d167be3ecc9ad56b8d2b640d1615852807f8da12795ba7fc91d0fcbfe77d1'
